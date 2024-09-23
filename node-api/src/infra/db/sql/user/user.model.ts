75ac3ab1a3b6534ce38bd5d22304ddf7c5a80455
Vinicius <vinicius.padua@tarvos.ag>
2024-09-23 00:40:08 -0300

    feat: created user model

---
+++ node-api/src/infra/db/sql/user/user.model.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Link } from "../link/link.model";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "varchar", length: 255 })
+  name: string;
+
+  @Column({ type: "varchar", length: 255 })
+  email: string;
+
+  @Column({ type: "varchar", length: 255 })
+  password: string;
+
+  // Relacionamento One-to-Many com a entidade Link
+  @OneToMany(() => Link, (link) => link.user)
+  links: Link[];
+}
