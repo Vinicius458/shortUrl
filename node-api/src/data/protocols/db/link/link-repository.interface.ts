d3dacbcab61a9d081a34bef31ee25c3e65df7bb7
Vinicius <vinicius.padua@tarvos.ag>
2024-09-22 22:30:56 -0300

    feat: created link repository interface

--- node-api/src/data/protocols/db/link/link-repository.interface.ts
+++ node-api/src/data/protocols/db/link/link-repository.interface.ts
@@ -1,5 +1,12 @@
 import Link from "@/domain/entities/link/link";
+import LinkInterface from "@/domain/entities/link/link.interface";
 
 export default interface LinkRepositoryInterface {
-  create(link: Link): Promise<void>;
+  create(link: Link): Promise<string>;
+
+  findAll(userId: string): Promise<Array<LinkInterface>>;
+
+  update(linkId: string, updateData: Partial<Link>): Promise<void>;
+
+  delete(linkId: string): Promise<void>;
 }
