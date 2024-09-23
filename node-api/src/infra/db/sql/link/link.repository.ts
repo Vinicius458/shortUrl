00cbeca83624241ea01a7dc3103b166de30c5333
Vinicius <vinicius.padua@tarvos.ag>
2024-09-22 22:11:55 -0300

    feat: created link repository

---
+++ node-api/src/infra/db/sql/link/link.repository.ts
@@ -0,0 +1,54 @@
+import { DataSource, Repository } from "typeorm";
+import { Link as LinkModel } from "../link/link.model";
+import LinkRepositoryInterface from "@/data/protocols/db/link/link-repository.interface";
+import Link from "@/domain/entities/link/link";
+import LinkInterface from "@/domain/entities/link/link.interface";
+
+export default class LinkRepository implements LinkRepositoryInterface {
+  private linkRepo: Repository<LinkModel>;
+  constructor(private dataSource: DataSource) {
+    this.linkRepo = this.dataSource.getRepository(LinkModel);
+  }
+
+  async create(linkEntity: Link): Promise<string> {
+    const link = this.linkRepo.create({
+      originalUrl: linkEntity.originaUrl,
+      shortenedUrl: linkEntity.shortenedUrl,
+      user: { id: linkEntity.userId },
+    });
+
+    const linkCreated = await this.linkRepo.save(link);
+    return linkCreated.id;
+  }
+
+  async findAll(userId: string): Promise<Array<LinkInterface>> {
+    const links = await this.linkRepo.find({ where: { user: { id: userId } } });
+    return links.map((link) => new Link(link.shortenedUrl, link.originalUrl));
+  }
+
+  async update(linkId: string, updateData: Partial<Link>): Promise<void> {
+    const link = await this.linkRepo.findOne({ where: { id: linkId } });
+
+    if (!link) {
+      throw new Error("Link not found");
+    }
+
+    if (updateData.originaUrl) link.originalUrl = updateData.originaUrl;
+    if (updateData.shortenedUrl) link.shortenedUrl = updateData.shortenedUrl;
+
+    // Salvar as alterações
+    await this.linkRepo.save(link);
+  }
+
+  async delete(linkId: string): Promise<void> {
+    // Verifica se o link existe
+    const link = await this.linkRepo.findOne({ where: { id: linkId } });
+
+    if (!link) {
+      throw new Error("Link not found");
+    }
+
+    // Remove o link
+    await this.linkRepo.remove(link);
+  }
+}
