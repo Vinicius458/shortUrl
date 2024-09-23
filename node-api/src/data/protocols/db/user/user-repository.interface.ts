f815fbc4102fd76f1297923ba7edfd873b5bc085
Vinicius <vinicius.padua@tarvos.ag>
2024-09-23 00:46:34 -0300

    feat : new method of UserRepository interface

--- node-api/src/data/protocols/db/user/user-repository.interface.ts
+++ node-api/src/data/protocols/db/user/user-repository.interface.ts
@@ -1,8 +1,7 @@
 import User from "@/domain/entities/user/user";
 
 export default interface UserRepositoryInterface {
-  find(userId: string): Promise<UserRepositoryInterface.Result>;
-}
-export namespace UserRepositoryInterface {
-  export type Result = User;
+  create(userEntity: User): Promise<string>;
+
+  find(userId: string): Promise<User | null>;
 }
