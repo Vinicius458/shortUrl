0f63bf2f8c8d59d8b323ac1d0b9f36ba1ae5a465
Vinicius <vinicius.padua@tarvos.ag>
2024-09-23 20:41:32 -0300

    feat: new account use case

---
+++ node-api/src/data/usecases/account/account.ts
@@ -0,0 +1,23 @@
+import { Account } from "@/domain/usecases";
+import { Hasher } from "@/data/protocols";
+import UserRepositoryInterface from "@/data/protocols/db/user/user-repository.interface";
+import User from "@/domain/entities/user/user";
+
+export class AccountUseCase implements Account {
+  constructor(
+    private readonly hasher: Hasher,
+    private readonly userRepository: UserRepositoryInterface
+  ) {}
+
+  async add(accountData: Account.Params): Promise<Account.Result> {
+    const exists = await this.userRepository.checkByEmail(accountData.email);
+    let isValid = false;
+    if (!exists) {
+      const hashedPassword = await this.hasher.hash(accountData.password);
+      const user = new User(accountData.name, accountData.email);
+      user.password = hashedPassword;
+      isValid = !!(await this.userRepository.create(user));
+    }
+    return isValid;
+  }
+}
