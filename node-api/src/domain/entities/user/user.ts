747a4081a8847cb4a9f7f9fd21fd17680893ea1b
Vinicius <vinicius.padua@tarvos.ag>
2024-09-24 20:48:43 -0300

    feat: add get and set user token

--- node-api/src/domain/entities/user/user.ts
+++ node-api/src/domain/entities/user/user.ts
@@ -1,12 +1,17 @@
 import UserInterface from "./user.interface";
 
 export default class User implements UserInterface {
+  private _id: string;
+  private _password: string;
+  private _token: string;
   constructor(
-    private _id: string,
     private _name: string,
     private _email: string
   ) {}
 
+  set id(key: string) {
+    this._id = key;
+  }
   get id(): string {
     return this._id;
   }
@@ -17,5 +22,19 @@ export default class User implements UserInterface {
 
   get email(): string {
     return this._email;
+  }
+  set password(hash: string) {
+    this._password = hash;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  set token(key: string) {
+    this._token = key;
+  }
+  get token(): string {
+    return this._token;
   }
 }
